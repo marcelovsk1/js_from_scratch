282fabf021c6f51ec8995213147fb54e50da2023
Marcelo Amaral Alves <marceloamaralalves@Marcelos-Mac-mini.local>
2025-04-12 22:55:02 -0300

    aula 2 concluida

--- JS_DO_ZERO/AULA 2/script.js
+++ JS_DO_ZERO/AULA 2/script.js
@@ -1,4 +1,21 @@
 function adicionarTarefa() {
-    let mensagem = "Tarefa adicionada com sucesso!";
-          document.getElementById("mensagem").textContent = mensagem;
+    const mensagem = "Tarefa adicionada com sucesso!";
+
+    const inputTarefa = document.getElementById("inputTarefa")
+    const tarefa = inputTarefa.value;
+
+        document.getElementById("mensagem").textContent = mensagem;
+        setTimeout(() => {
+            document.getElementById("mensagem").textContent = "";
+        }, 3000);
+
+    const listaTarefas = document.getElementById("listaTarefas");
+    const novaTarefa = document.createElement("li");
+        novaTarefa.textContent = tarefa;
+        if (tarefa === "") {
+            document.getElementById("mensagem").textContent = "Digite uma tarefa!";
+        } else {
+            listaTarefas.appendChild(novaTarefa);
+            inputTarefa.value = "";
+        }
   }
